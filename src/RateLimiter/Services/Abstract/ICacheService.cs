755c5554ba2e3868bb171c96171eeb28e8e9c1b5
zaminalili <zaminalili29@gmail.com>
2025-02-07 22:54:38 +0400

    Add ICacheService

---
+++ src/RateLimiter/Services/Abstract/ICacheService.cs
@@ -0,0 +1,7 @@
+﻿namespace RateLimiter.Services.Abstract;
+
+public interface ICacheService
+{
+    Task<string?> GetStringAsync(string key);
+    Task SetStringAsync(string key, string value, TimeSpan absoluteTimeInterval, TimeSpan? slidingTimeInterval = null);
+}
