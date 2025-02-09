b657fe4df57f9af6ce30df4ba12c1ab1802f3949
zaminalili <zaminalili29@gmail.com>
2025-02-09 14:06:00 +0400

    Switch from IDistributedCache to ICacheService

--- src/RateLimiter/Services/Concrete/RateLimiterService.cs
+++ src/RateLimiter/Services/Concrete/RateLimiterService.cs
@@ -1,34 +1,30 @@
-﻿using Microsoft.Extensions.Caching.Distributed;
-using Microsoft.Extensions.Options;
-using RateLimiter.Configure;
+﻿using Microsoft.Extensions.Options;
+using RateLimiter.Options;
 using RateLimiter.Services.Abstract;
 
 namespace RateLimiter.Services.Concrete;
 
-internal class RateLimiterService(IOptions<RateLimiterOptions> options, IDistributedCache cache) : IRateLimiterService
+internal class RateLimiterService(IOptions<RateLimiterOptions> options, ICacheService cacheService) : IRateLimiterService
 {
-    private readonly int _maxRequestCount = options.Value.MaxRequestCount;
-    private readonly TimeSpan _timeInterval = options.Value.TimeInterval;
+    private readonly int maxRequestCount = options.Value.MaxRequestCount;
+    private readonly TimeSpan timeInterval = options.Value.TimeInterval;
 
     private async Task<int> IncreaseAsync(string key, string? value)
     {
         int counter = string.IsNullOrEmpty(value) ? 0 : int.Parse(value);
         counter++;
 
-        await cache.SetStringAsync(key, counter.ToString(), new DistributedCacheEntryOptions
-        {
-            AbsoluteExpirationRelativeToNow = _timeInterval
-        });
+        await cacheService.SetStringAsync(key, counter.ToString(), timeInterval);
 
         return counter;
     }
 
     public async Task<bool> IsRateLimitedAsync(string ipAddress)
     {
-        var value = await cache.GetStringAsync(ipAddress);
+        var value = await cacheService.GetStringAsync(ipAddress);
 
         int counter = await IncreaseAsync(ipAddress, value);
 
-        return counter > _maxRequestCount;
+        return counter > maxRequestCount;
     }
 }
